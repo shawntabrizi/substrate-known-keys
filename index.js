f8c61505d66d92a65d0dcc1e3897639c442c7eac
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-09-06 20:22:46 -0400

    get keys from all networks

--- index.js
+++ index.js
@@ -3,45 +3,20 @@ var { stringToHex } = require('@polkadot/util');
 var { xxhashAsHex } = require('@polkadot/util-crypto');
 const dictionary = require("./dictionary.json");
 
+// This is a list of the "well_known_keys" in Substrate
+let output = [];
+
+// We will grab the metadata for all these networks.
+// Does not handle failure well...
+let networks = [
+	"wss://kusama-rpc.polkadot.io/",
+	"wss://rpc.polkadot.io/",
+	"wss://kusama-statemine-rpc.paritytech.net",
+	"wss://rococo-rpc.polkadot.io",
+	"wss://westend-rpc.polkadot.io",
+]
+
 async function main() {
-
-	var args = process.argv;
-	let option = args[2] || "";
-
-	let network;
-	switch(option.toLowerCase()) {
-		case "":
-		case "kusama":
-			network = "wss://kusama-rpc.polkadot.io/";
-			break;
-		case "polkadot":
-			network = "wss://rpc.polkadot.io/";
-			break;
-		case "local":
-			network = "ws://127.0.0.1:9944";
-			break;
-		default:
-			network = option;
-	}
-
-	const provider = new WsProvider(network, false);
-	provider.connect();
-
-	const api = await ApiPromise.create({ provider });
-
-	// Get general information about the node we are connected to
-	const [chain, nodeName, nodeVersion] = await Promise.all([
-		api.rpc.system.chain(),
-		api.rpc.system.name(),
-		api.rpc.system.version()
-	]);
-	console.log(
-		`Getting keys for ${chain} using ${nodeName} v${nodeVersion} @ ${network}`
-	);
-
-	// This is a list of the "well_known_keys" in Substrate
-	let output = [];
-
 	output = output.concat(dictionary.raw);
 
 	for (word of dictionary.encoded) {
@@ -60,25 +35,14 @@ async function main() {
 		});
 	}
 
-	for (module in api.query) {
-		if (module == "substrate") { continue; }
+	await queryNetworks();
 
-		for (storage in api.query[module]) {
-			let query = api.query[module][storage];
-			try {
-				output.push({
-					"name": module + " " + storage,
-					"key": query.key(),
-				});
-			} catch {
-				output.push({
-					"name": module + " " + storage,
-					"key": query.keyPrefix(),
-				});
-			}
-		}
-	}
-
+	// Remove duplicates
+	// TODO: optimize in the original creation of output
+	output = output.filter((thing, index, self) =>
+		index === self.findIndex((t) => (
+			t.name === thing.name && t.key === thing.key
+		)));
 	output.sort(sortByModule);
 
 	const fs = require('fs')
@@ -87,7 +51,47 @@ async function main() {
 		JSON.stringify(output, null, '  ')
 	);
 
-	provider.disconnect()
+}
+
+// Go through each network and query the information, adding it to `output`.
+async function queryNetworks() {
+	for (network of networks) {
+		const provider = new WsProvider(network, false);
+		provider.connect();
+
+		const api = await ApiPromise.create({ provider });
+
+		// Get general information about the node we are connected to
+		const [chain, nodeName, nodeVersion] = await Promise.all([
+			api.rpc.system.chain(),
+			api.rpc.system.name(),
+			api.rpc.system.version()
+		]);
+		console.log(
+			`Getting keys for ${chain} using ${nodeName} v${nodeVersion} @ ${network}`
+		);
+
+		for (module in api.query) {
+			if (module == "substrate") { continue; }
+
+			for (storage in api.query[module]) {
+				let query = api.query[module][storage];
+				try {
+					output.push({
+						"name": module + " " + storage,
+						"key": query.key(),
+					});
+				} catch {
+					output.push({
+						"name": module + " " + storage,
+						"key": query.keyPrefix(),
+					});
+				}
+			}
+		}
+
+		provider.disconnect()
+	}
 }
 
 function sortByModule(a, b) {
